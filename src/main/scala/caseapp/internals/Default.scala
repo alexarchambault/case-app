474186efc0a15f7863f319f1dc21a8276ac2cc69
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-01-01 23:58:44 +0100

    Missing implicit

--- src/main/scala/caseapp/internals/Default.scala
+++ src/main/scala/caseapp/internals/Default.scala
@@ -17,6 +17,7 @@ object Default {
   implicit def default[CC <: Product : TypeTag]: Default[CC] =
     Default(util.instantiateCCWithDefaultValues[CC])
 
+  implicit val unitDefault: Default[Unit] = Default(())
   implicit val intDefault: Default[Int] = Default(0)
   implicit val stringDefault: Default[String] = Default("")
 
