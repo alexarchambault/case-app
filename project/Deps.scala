7c9bccfb40410200003fec24b3f2d38e239ff3c3
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-07-06 18:10:59 +0200

    Switch to scalatest 3.0.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,7 +12,7 @@ object Deps {
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.patch
   def scalaCompiler = setting("org.scala-lang" % "scala-compiler" % scalaVersion.value)
   def scalaReflect = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
-  def scalaTest = setting("org.scalatest" %%% "scalatest" % "3.0.1")
+  def scalaTest = setting("org.scalatest" %%% "scalatest" % "3.0.3")
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
 
 }
