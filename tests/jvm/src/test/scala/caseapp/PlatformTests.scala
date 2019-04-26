319fe0a28226f248b30e744241efdb3e81dce7d6
杨博 (Yang Bo) <pop.atry@gmail.com>
2019-04-27 01:51:06 +0800

    Support ArgParser for java.nio.file.Path

--- tests/jvm/src/test/scala/caseapp/PlatformTests.scala
+++ tests/jvm/src/test/scala/caseapp/PlatformTests.scala
@@ -2,6 +2,7 @@ package caseapp
 
 import java.util.{Calendar, GregorianCalendar}
 import utest._
+import java.nio.file._
 
 object PlatformTests extends TestSuite {
 
@@ -11,11 +12,26 @@ object PlatformTests extends TestSuite {
 
   CaseApp.parseWithHelp[WithCalendar] _
 
+  final case class WithPath(
+    path: Path
+  )
+
+  CaseApp.parseWithHelp[WithPath] _
+
   val tests = TestSuite {
     "parse a date" - {
       val res = Parser[WithCalendar].parse(Seq("--date", "2014-10-23"))
       val expectedRes = Right((
         WithCalendar(date = new GregorianCalendar(2014, 9, 23)),
+        Nil
+      ))
+      assert(res == expectedRes)
+    }
+
+    "parse a path" - {
+      val res = Parser[WithPath].parse(Seq("--path", "/path/to/file.ext"))
+      val expectedRes = Right((
+        WithPath(path = Paths.get("/path/to/file.ext")),
         Nil
       ))
       assert(res == expectedRes)
