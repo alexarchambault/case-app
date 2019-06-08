4772822742851f40a798195f9d2ecd4a4a46ae9b
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-06-08 16:16:11 +0200

    Switch to scala 2.13.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -8,7 +8,7 @@ object Settings {
 
   def scala211 = "2.11.12"
   private def scala212 = "2.12.8"
-  private def scala213 = "2.13.0-RC1"
+  private def scala213 = "2.13.0"
 
   lazy val shared = Seq(
     scalaVersion := scala212,
