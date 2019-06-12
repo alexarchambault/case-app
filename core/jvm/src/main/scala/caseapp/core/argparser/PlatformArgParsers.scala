bd7c81a8fbec35eda6165f4f596bdd0adbffc077
杨博 (Yang Bo) <pop.atry@gmail.com>
2019-06-12 10:42:54 -0700

    Support ArgParser for java.nio.file.Path (#114)

--- core/jvm/src/main/scala/caseapp/core/argparser/PlatformArgParsers.scala
+++ core/jvm/src/main/scala/caseapp/core/argparser/PlatformArgParsers.scala
@@ -1,11 +1,23 @@
 package caseapp.core.argparser
 
+import java.nio.file.{InvalidPathException, Path, Paths}
 import java.text.{ParseException, SimpleDateFormat}
 import java.util.{Calendar, GregorianCalendar}
 
 import caseapp.core.Error
 
 abstract class PlatformArgParsers {
+
+  implicit val path: ArgParser[Path] = {
+    SimpleArgParser.from("path/to/file") { pathString =>
+      try {
+        Right(Paths.get(pathString))
+      } catch {
+        case e: InvalidPathException =>
+          Left(Error.MalformedValue("path", e.getMessage))
+      }
+    }
+  }
 
   implicit val calendar: ArgParser[Calendar] =
     SimpleArgParser.from("yyyy-MM-dd") { s =>
