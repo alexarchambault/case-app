80dedf9c65e81d729217ad99144662d02365d03c
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-09-21 15:32:15 +0200

    Switch to scala 2.12.10

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ import Aliases._
 object Settings {
 
   def scala211 = "2.11.12"
-  private def scala212 = "2.12.8"
+  private def scala212 = "2.12.10"
   private def scala213 = "2.13.0"
 
   lazy val shared = Seq(
