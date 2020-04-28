f219484349317baf23a8afa48f43a674e2a36c3a
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-04-28 17:19:52 +0200

    Update scala to 2.13.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ import Aliases._
 object Settings {
 
   private def scala212 = "2.12.11"
-  private def scala213 = "2.13.1"
+  private def scala213 = "2.13.2"
 
   private lazy val isAtLeastScala213 = Def.setting {
     import Ordering.Implicits._
