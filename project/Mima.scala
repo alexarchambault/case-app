5e47cb021a863f97c362bfed9443ee1932c5c1e9
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-06-06 17:55:15 +0200

    Reset MiMA
    Some internal methods changes mostly. We should be source compatible
    with 2.0.x overall.

--- project/Mima.scala
+++ project/Mima.scala
@@ -9,7 +9,7 @@ import scala.sys.process._
 object Mima {
 
   def binaryCompatibilityVersions: Set[String] =
-    Seq("git", "tag", "--merged", "HEAD^", "--contains", "da290601")
+    Seq("git", "tag", "--merged", "HEAD^", "--contains", "a06e9001")
       .!!
       .linesIterator
       .map(_.trim)
