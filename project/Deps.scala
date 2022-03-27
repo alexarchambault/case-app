42c1c3dd9bdbfdd9b205c5c5a2d3a7f080f84ec6
Scala Steward <me@scala-steward.org>
2022-03-27 22:05:01 +0200

    Update shapeless to 2.3.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
   def refined       = setting("eu.timepit" %%% "refined" % "0.9.28")
   def scalaCompiler = setting("org.scala-lang" % "scala-compiler" % scalaVersion.value)
   def scalaReflect  = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
-  def shapeless     = setting("com.chuusai" %%% "shapeless" % "2.3.8")
+  def shapeless     = setting("com.chuusai" %%% "shapeless" % "2.3.9")
   def sourcecode    = setting("com.lihaoyi" %%% "sourcecode" % "0.2.8")
   def utest         = setting("com.lihaoyi" %%% "utest" % "0.7.11")
 
