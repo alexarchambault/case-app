526d8885dc11bd059b93c939b9adf036357ffc11
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-06-20 18:31:39 +0200

    Update refined to 0.9.29 (#390)

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
   def catsEffect3   = setting("org.typelevel" %%% "cats-effect" % "3.3.9")
   def dataClass     = "io.github.alexarchambault" %% "data-class" % "0.2.5"
   def macroParadise = "org.scalamacros"            % "paradise"   % "2.1.1" cross CrossVersion.patch
-  def refined       = setting("eu.timepit" %%% "refined" % "0.9.28")
+  def refined       = setting("eu.timepit" %%% "refined" % "0.9.29")
   def scalaCompiler = setting("org.scala-lang" % "scala-compiler" % scalaVersion.value)
   def scalaReflect  = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
   def shapeless     = setting("com.chuusai" %%% "shapeless" % "2.3.9")
