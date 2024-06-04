2047062efe23eabd4f8d8b7e20791741a8acd35b
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-06-04 15:20:57 +0200

    Update PPrint to 0.9.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
   def catsEffect3   = setting("org.typelevel" %%% "cats-effect" % "3.5.4")
   def dataClass     = "io.github.alexarchambault" %% "data-class" % "0.2.6"
   def macroParadise = "org.scalamacros"            % "paradise"   % "2.1.1" cross CrossVersion.patch
-  def pprint        = setting("com.lihaoyi" %%% "pprint" % "0.8.1")
+  def pprint        = setting("com.lihaoyi" %%% "pprint" % "0.9.0")
   def scalaCompiler = setting("org.scala-lang" % "scala-compiler" % scalaVersion.value)
   def scalaReflect  = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
   def shapeless     = setting("com.chuusai" %%% "shapeless" % "2.3.12")
