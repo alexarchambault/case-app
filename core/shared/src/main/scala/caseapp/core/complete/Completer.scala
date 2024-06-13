2daa96ce2164002c42d42fd709554f030c3a7ed4
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-06-13 20:11:21 +0200

    Add Completer#completeOptionValue helper

--- core/shared/src/main/scala/caseapp/core/complete/Completer.scala
+++ core/shared/src/main/scala/caseapp/core/complete/Completer.scala
@@ -26,6 +26,58 @@ trait Completer[-T] { self =>
 
 object Completer {
 
+  implicit class CompleterOps[T](private val completer: Completer[T]) extends AnyVal {
+    def completeOptionValue(f: (
+      Arg,
+      String,
+      Option[T],
+      RemainingArgs
+    ) => Option[List[CompletionItem]]): Completer[T] =
+      WithOptionValue(completer, f)
+  }
+
+  class DefaultTo[-T](default: Completer[T]) extends Completer[T] {
+    override def optionName(
+      prefix: String,
+      state: Option[T],
+      args: RemainingArgs
+    ): List[CompletionItem] =
+      default.optionName(prefix, state, args)
+    override def optionValue(
+      arg: Arg,
+      prefix: String,
+      state: Option[T],
+      args: RemainingArgs
+    ): List[CompletionItem] =
+      default.optionValue(arg, prefix, state, args)
+    override def argument(
+      prefix: String,
+      state: Option[T],
+      args: RemainingArgs
+    ): List[CompletionItem] =
+      default.argument(prefix, state, args)
+    override def postDoubleDash(state: Option[T], args: RemainingArgs): Option[Completer[T]] =
+      default.postDoubleDash(state, args)
+
+    override def toString(): String =
+      s"Completer.DefaultTo($default)"
+  }
+
+  private final case class WithOptionValue[T](
+    self: Completer[T],
+    f: (Arg, String, Option[T], RemainingArgs) => Option[List[CompletionItem]]
+  ) extends DefaultTo[T](self) {
+    override def optionValue(
+      arg: Arg,
+      prefix: String,
+      state: Option[T],
+      args: RemainingArgs
+    ): List[CompletionItem] =
+      f(arg, prefix, state, args).getOrElse {
+        super.optionValue(arg, prefix, state, args)
+      }
+  }
+
   private final case class Mapped[T, U](self: Completer[T], f: U => Option[T])
       extends Completer[U] {
     def optionName(prefix: String, state: Option[U], args: RemainingArgs): List[CompletionItem] =
