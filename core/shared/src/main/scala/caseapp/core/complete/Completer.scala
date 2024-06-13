e7a478c8b81ecd1c7b3130377891f5d665b18118
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-06-13 20:10:27 +0200

    NIT Better toString for Completer#contramap instances

--- core/shared/src/main/scala/caseapp/core/complete/Completer.scala
+++ core/shared/src/main/scala/caseapp/core/complete/Completer.scala
@@ -16,25 +16,31 @@ trait Completer[-T] { self =>
   def postDoubleDash(state: Option[T], args: RemainingArgs): Option[Completer[T]] =
     None
 
-  def contramapOpt[U](f: U => Option[T]): Completer[U] =
-    new Completer[U] {
-      def optionName(prefix: String, state: Option[U], args: RemainingArgs): List[CompletionItem] =
-        self.optionName(prefix, state.flatMap(f), args)
-      def optionValue(
-        arg: Arg,
-        prefix: String,
-        state: Option[U],
-        args: RemainingArgs
-      ): List[CompletionItem] =
-        self.optionValue(arg, prefix, state.flatMap(f), args)
-      def argument(prefix: String, state: Option[U], args: RemainingArgs): List[CompletionItem] =
-        self.argument(prefix, state.flatMap(f), args)
-
-      override def postDoubleDash(state: Option[U], args: RemainingArgs): Option[Completer[U]] =
-        self.postDoubleDash(state.flatMap(f), args).map(_.contramapOpt(f))
-    }
+  final def contramapOpt[U](f: U => Option[T]): Completer[U] =
+    Completer.Mapped(this, f)
   def withHelp: Completer[WithHelp[T]] =
     contramapOpt(_.baseOrError.toOption)
   def withFullHelp: Completer[WithFullHelp[T]] =
     contramapOpt(_.withHelp.baseOrError.toOption)
+}
+
+object Completer {
+
+  private final case class Mapped[T, U](self: Completer[T], f: U => Option[T])
+      extends Completer[U] {
+    def optionName(prefix: String, state: Option[U], args: RemainingArgs): List[CompletionItem] =
+      self.optionName(prefix, state.flatMap(f), args)
+    def optionValue(
+      arg: Arg,
+      prefix: String,
+      state: Option[U],
+      args: RemainingArgs
+    ): List[CompletionItem] =
+      self.optionValue(arg, prefix, state.flatMap(f), args)
+    def argument(prefix: String, state: Option[U], args: RemainingArgs): List[CompletionItem] =
+      self.argument(prefix, state.flatMap(f), args)
+
+    override def postDoubleDash(state: Option[U], args: RemainingArgs): Option[Completer[U]] =
+      self.postDoubleDash(state.flatMap(f), args).map(_.contramapOpt(f))
+  }
 }
